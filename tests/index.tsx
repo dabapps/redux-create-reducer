94e25abca6537b9ae710716f0c80083f853cd115
Jake 'Sid' Smith <jake@dabapps.com>
2019-03-19 22:10:55 +0000

    Add initial state test

--- tests/index.tsx
+++ tests/index.tsx
@@ -2,8 +2,20 @@ import { createReducer } from '../src';
 
 describe('createReducer', () => {
   it('returns a reducer function', () => {
-    const result = createReducer({}, {});
+    const reducer = createReducer({}, {});
 
-    expect(typeof result).toBe('function');
+    expect(typeof reducer).toBe('function');
+  });
+
+  it('returns the initialState when an unknown action is emitted', () => {
+    const reducer = createReducer<number>(
+      {
+        foo: state => state + 1,
+      },
+      0
+    );
+
+    expect(reducer(undefined, { type: 'bar' })).toBe(0);
+    expect(reducer(undefined, { type: 'foo' })).toBe(1);
   });
 });
