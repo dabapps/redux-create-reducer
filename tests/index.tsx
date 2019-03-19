83335cae0183f18778410357c36c5e410a1ac71d
Jake 'Sid' Smith <jake@dabapps.com>
2019-03-19 22:13:03 +0000

    Test invalid keys

--- tests/index.tsx
+++ tests/index.tsx
@@ -18,4 +18,18 @@ describe('createReducer', () => {
     expect(reducer(undefined, { type: 'bar' })).toBe(0);
     expect(reducer(undefined, { type: 'foo' })).toBe(1);
   });
+
+  it('should error if invalid handler keys are used', () => {
+    const MATCHES_INVALID_KEY = /invalid/i;
+
+    expect(() => createReducer({ undefined: () => ({}) }, {})).toThrow(
+      MATCHES_INVALID_KEY
+    );
+    expect(() => createReducer({ null: () => ({}) }, {})).toThrow(
+      MATCHES_INVALID_KEY
+    );
+    expect(() => createReducer({ okay: () => ({}) }, {})).not.toThrow(
+      MATCHES_INVALID_KEY
+    );
+  });
 });
