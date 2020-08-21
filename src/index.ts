39e88c68c777ee8482afda2c6c80c243ca630680
Jake 'Sid' Smith <jake@dabapps.com>
2020-08-21 10:48:46 +0100

    Disallow AnyAction and better action type inference

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import { Action, AnyAction, Reducer } from 'redux';
+import { Action, Reducer } from 'redux';
 
 const INVALID_HANDLER_KEYS = ['undefined', 'null'];
 
 type NotUndefined = {} | null;
 
-export type Handlers<S extends NotUndefined, A extends Action = AnyAction> = {
-  [P in A['type']]: (state: S, action: A) => S;
-};
+type ActionOfType<A extends Action, T> = Exclude<A, Exclude<A, { type: T }>>;
 
-function validateKeys(handlers: Handlers<any, any>) {
+function validateKeys(handlers: Record<string, unknown>) {
   INVALID_HANDLER_KEYS.forEach(key => {
     if (Object.prototype.hasOwnProperty.call(handlers, key)) {
       throw new Error(`Invalid createReducer handler key: ${key}`);
@@ -18,11 +16,18 @@ function validateKeys(handlers: Handlers<any, any>) {
 
 export function createReducer<
   S extends NotUndefined,
-  A extends Action = AnyAction
->(handlers: Handlers<S, A>, initialState: S): Reducer<S, A> {
+  A extends Action<T>,
+  T extends string
+>(
+  handlers: {
+    [P in T]: (state: S, action: ActionOfType<A, P>) => S;
+  },
+  initialState: S
+): Reducer<S, ActionOfType<A, T>> {
   if (
     !handlers ||
-    typeof (handlers as Handlers<S, A> | undefined) !== 'object' ||
+    // tslint:disable-next-line:strict-type-predicates
+    typeof handlers !== 'object' ||
     Array.isArray(handlers)
   ) {
     throw new Error(
@@ -36,11 +41,12 @@ export function createReducer<
 
   validateKeys(handlers);
 
-  return (state: S = initialState, action: A): S => {
+  return (state: S = initialState, action: ActionOfType<A, T>): S => {
     const { type } = action;
 
     if (Object.prototype.hasOwnProperty.call(handlers, type)) {
-      return handlers[type as keyof Handlers<S, A>](state, action);
+      const handler = handlers[type];
+      return handler(state, action);
     }
 
     return state;
